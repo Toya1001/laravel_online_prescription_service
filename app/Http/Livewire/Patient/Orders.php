23317ce30f585bf9a4d4432b6b8731f493dbea6f
Latoya Jackson <jacksonlatoya04@gmail.com>
2022-05-01 20:55:45 -0500

    updates

--- app/Http/Livewire/Patient/Orders.php
+++ app/Http/Livewire/Patient/Orders.php
@@ -3,17 +3,48 @@
 namespace App\Http\Livewire\Patient;
 
 use App\Models\Patient;
+use App\Models\Prescription;
+use App\Models\DrugInventory;
 use App\Models\PrescriptionOrder;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class Orders extends Component
 {
+    public $viewInfo = false;
+    
+    public function viewPrescription($id)
+    {
+        $this->viewInfo = true;
+        $this->singlePrescription = Prescription::find($id);
+    }
+
+    public function refillOrder($id)
+    {
+        $quantity = Prescription::where('id', $id)->value('quantity');
+        $drugId = Prescription::where('id', $id)->value('drug_id');
+        $inventory = DrugInventory::where('drug_id', $drugId)->value('quantity');
+        $refillNo = Prescription::where('id', $id)->value('repeat');
+        $orders = PrescriptionOrder::where('prescription_id', $id)->count();
+        if ($orders > $refillNo) {
+            session()->flash('message', 'Reached Refill limit. Unable to process order');
+        } else {
+            if ($quantity <= $inventory) {
+                PrescriptionOrder::create([
+                    'prescription_id' => $id,
+                    'patient_id' => Patient::where('user_id', Auth::id())->value('id'),
+                    'status' => 'Pending',
+                ]);
+            } else{
+                session()->flash('message', 'Drug out of stock'); 
+            }
+        }
+    }
     public function render()
     {
         $patientId= Patient::where('user_id', Auth::id())->value('id');
         return view('livewire.patient.orders',[
-            'orders' => PrescriptionOrder:: with('prescription', 'patient')->where('patientid', $patientId)->get(),
+            'orders' => PrescriptionOrder:: with('prescription', 'patient')->where('patient_id', $patientId)->get(),
         ]);
     }
 }
--- resources/views/livewire/patient/orders.blade.php
+++ resources/views/livewire/patient/orders.blade.php
@@ -1,4 +1,88 @@
 <div>
+      @if($viewInfo)
+      <div class="overflow-y-auto fixed inset-0 z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+
+          <div class="
+          flex
+          items-end
+          justify-center
+          min-h-screen
+          px-4
+          pt-4
+          pb-20
+          text-center
+          sm:block sm:p-0
+        ">
+              <div class="transition-opacity bg-gray-500 bg-opacity-75" aria-hidden="true"></div>
+              <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">​</span>
+              <div class="
+            inline-block
+            p-5
+            overflow-hidden
+            text-left
+            align-bottom
+            transition-all
+            transform
+            bg-white
+            rounded-lg
+            shadow-2xl
+            lg:p-16
+            sm:my-8 sm:align-middle sm:max-w-xl sm:w-full
+          ">
+                  <div>
+                      <div class="text-center flex justify-end"><i wire:click="$set('viewInfo', false)" class="fas fa-times text-2xl cursor-pointer"></i></div>
+
+                      <h2 class="
+                  mb-4
+                  text-2xl
+                  font-semibold
+                 
+                  text-black
+                  uppercase
+                  text-center
+                "> Prescription </h2>
+
+                      <div class="mt-3 text-left sm:mt-5">
+                          <h2 class="
+                  mb-4
+                  text-2xl
+                  font-semibold
+                  tracking-widest
+                  text-black
+                  uppercase
+                "> {{ $singlePrescription->patient->user->fname }} {{ $singlePrescription->patient->user->lname }} </h2>
+
+                          <h5 class="
+                  mb-8
+                  text-lg
+                  font-semibold
+                  leading-none
+                  tracking-tighter
+                  text-neutral-600
+                "> {{ $singlePrescription->patient->address }} {{ $singlePrescription->patient->city }}, {{ $singlePrescription->patient->parish }}</h5>
+                          <h4 class="
+                  mb-8
+                  text-xl
+                  font-medium
+                  leading-none
+                  tracking-tighter
+                  text-neutral-600">{{ $singlePrescription->drug->drug_name }} </h4>
+                          <p class="mx-auto text-base leading-relaxed text-black"> <span class="font-medium">Dosage: </span>{{ $singlePrescription->dosage }} </p>
+                          <p class="mx-auto text-base leading-relaxed text-black"> <span class="font-medium">Directions: </span>{{ $singlePrescription->directions }} </p>
+                          <p class="mx-auto text-base leading-relaxed text-black"> <span class="font-medium">Quantity: </span>{{ $singlePrescription->quantity }} </p>
+                          <p class="mx-auto text-base leading-relaxed text-black"> <span class="font-medium">Duration: </span>{{ $singlePrescription->duration }} </p>
+                          <p class="mx-auto text-base leading-relaxed text-black"> <span class="font-medium">Repeat: </span>{{ $singlePrescription->repeat }} </p>
+                      </div>
+                  </div>
+                  <div class="mt-6 sm:flex">
+                      <button wire:click="refillOrder({{ $singlePrescription->id }})" class="border border-indigo-500 bg-black text-white rounded-md mb-1 px-4 py-2 mx-8 mt-4 transition duration-500 ease select-none hover:bg-indigo-600 focus:outline-none focus:shadow-outline">Request Refill</button>
+                  </div>
+              </div>
+          </div>
+      </div>
+
+      @endif
+
     <section>
         <div class="px-4 py-12 mx-auto">
             <div class="max-w-4xl pt-4 mx-auto">
@@ -7,7 +91,7 @@
                         {{-- <div class="w-full border-t border-black"></div> --}}
                     </div>
                     <div class="relative flex justify-start">
-                        <span class="pr-3 text-2xl font-bold text-neutral-600 "> All Prescriptions </span>
+                        <span class="pr-3 text-2xl font-bold text-neutral-600 "> All Orders </span>
                     </div>
                 </div>
                 @foreach($orders as $prescriptions)
@@ -17,18 +101,20 @@
                             <img class="w-full rounded-md lg:h-32 lg:w-32" src="https://images.unsplash.com/photo-1616651181620-9906d6e43fc3?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8cGF0dGVybnxlbnwwfDJ8MHx8&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=900&amp;q=60" alt="text">
                         </div> --}}
                         <div>
-                            <span class="text-sm text-gray-500">{{ $prescriptions->rx_no}}</span>
+                            <span class="text-sm text-gray-500">{{ $prescriptions->prescription->rx_no}}</span>
+                            <p class="text-sm text-gray-500"><span class="font-medium">Order #: </span>{{ $prescriptions->id}}</p>
+
                             <p class="mt-3 text-lg font-medium leading-6">
-                                <button wire:click="viewPrescription({{ $prescriptions->id }})" class="
+                                <button wire:click="viewPrescription({{ $prescriptions->prescription->id }})" class="
                       text-xl font-medium text-neutral-600
                       group-hover:text-blue-400
                       lg:text-2xl
-                    ">{{ $prescriptions->drug->drug_name }} </button>
+                    ">{{ $prescriptions->prescription->drug->drug_name }} </button>
 
                             </p>
-                            <p class="mt-2 text-lg text-gray-500"> {{ $prescriptions->dosage }} </p>
-                            <p class="mt-2 text-lg text-gray-500"> {{ $prescriptions->directions }} </p>
-                            <p class="mt-2 text-lg text-gray-500"> {{ $prescriptions->repeat}} </p>
+                            <p class="mt-2 text-lg text-gray-500"> {{ $prescriptions->prescription->dosage }} </p>
+                            <p class="mt-2 text-lg text-gray-500"> {{ $prescriptions->prescription->directions }} </p>
+                            <p class="mt-2 text-lg text-gray-500"> {{ $prescriptions->prescription->repeat}} </p>
 
 
                         </div>
