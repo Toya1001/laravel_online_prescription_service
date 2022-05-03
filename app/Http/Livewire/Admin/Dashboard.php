fefbe4da8f37f87b4764a464f68b86e370cca6c7
Latoya Jackson <jacksonlatoya04@gmail.com>
2022-05-03 10:28:29 -0500

    update admin dashboard

--- app/Http/Livewire/Admin/Dashboard.php
+++ app/Http/Livewire/Admin/Dashboard.php
@@ -2,12 +2,26 @@
 
 namespace App\Http\Livewire\Admin;
 
+use App\Models\Doctor;
+use App\Models\DrugInventory;
+use App\Models\Patient;
+use App\Models\Prescription;
+use App\Models\PrescriptionOrder;
 use Livewire\Component;
 
 class Dashboard extends Component
 {
     public function render()
     {
-        return view('livewire.admin.dashboard');
+        $patients= Patient::count();
+        $prescriptions = Prescription::count();
+        $doctors = Doctor::count();
+        $inventory = DrugInventory::sum('quantity');
+        return view('livewire.admin.dashboard', [
+            'patients' =>$patients,
+            'prescriptions' =>$prescriptions,
+            'doctors' => $doctors,
+            'inventory' =>$inventory,
+        ]);
     }
 }
