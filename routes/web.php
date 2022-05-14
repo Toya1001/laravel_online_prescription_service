392e6e8c6c0a8302c727b74aa03fece263992d2e
Latoya Jackson <jacksonlatoya04@gmail.com>
2022-05-14 11:58:25 -0500

    updates

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::view('/','home');
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
