4fb165db7a7c707a9186e334fa72f0f19dc9eae4
Latoya Jackson <jacksonlatoya04@gmail.com>
2022-05-27 07:04:07 -0500

    update api

--- app/Http/Livewire/Admin/Patients.php
+++ app/Http/Livewire/Admin/Patients.php
@@ -42,7 +42,7 @@ class Patients extends Component
     public function createPatient()
     {
         $this->validate();
-        Http::withToken('1|4hOjHuSZBHFlMtftXYeAJ3jTfQhdUdEiaF5GGmy6')->post('http://192.168.0.5:8080/api/user', [
+        Http::post('http://latoya.fimijm.com/eprescriptionapi/api/user', [
             'fname' => $this->firstName,
             'lname' => $this->lastName,
             'email' => $this->email,
@@ -50,11 +50,11 @@ class Patients extends Component
             'password' => Hash::make('password')
         ]);
 
-        $data = Http::withToken('1|4hOjHuSZBHFlMtftXYeAJ3jTfQhdUdEiaF5GGmy6')->get('http://192.168.0.5:8080/api/user')->json();
+        $data = Http::get('http://latoya.fimijm.com/eprescriptionapi/api/user')->json();
         $data = end($data);
         $userId = reset($data);
        
-        Http::withToken('1|4hOjHuSZBHFlMtftXYeAJ3jTfQhdUdEiaF5GGmy6')->post('http://192.168.0.5:8080/api/patient', [
+        Http::post('http://latoya.fimijm.com/eprescriptionapi/api/patient', [
             'user_id' => $userId,
             'mx_no' => $this->mxNo,
             'dob' => $this->dob,
@@ -71,7 +71,7 @@ class Patients extends Component
     public function editPatient($id)
     {
         $this->patientEdit = true;
-        $info = Http::withToken('1|4hOjHuSZBHFlMtftXYeAJ3jTfQhdUdEiaF5GGmy6')->get('http://192.168.0.5:8080/api/patient/' . $id)->json();
+        $info = Http::get('http://latoya.fimijm.com/eprescriptionapi/api/patient/' . $id)->json();
         // dd($info);
         $this->dob = $info['Patient']['dob'];
         $this->gender = $info['Patient']['gender'];
@@ -85,7 +85,7 @@ class Patients extends Component
         $this->patientID = $id;
 
 
-        $data = Http::withToken('1|4hOjHuSZBHFlMtftXYeAJ3jTfQhdUdEiaF5GGmy6')->get('http://192.168.0.5:8080/api/user/' . $this->userID)->json();
+        $data = Http::get('http://latoya.fimijm.com/eprescriptionapi/api/user/' . $this->userID)->json();
         $this->firstName = $data['fname'];
         $this->lastName = $data['lname'];
         $this->email = $data['email'];
@@ -95,13 +95,13 @@ class Patients extends Component
     {
         dd($this->userID);
         // $this->validate();
-        Http::withToken('1|4hOjHuSZBHFlMtftXYeAJ3jTfQhdUdEiaF5GGmy6')->put('http://192.168.0.5:8080/api/user/' . $this->userID, [
+        Http::put('http://latoya.fimijm.com/eprescriptionapi/api/user/' . $this->userID, [
             'fname' => $this->firstName,
             'lname' => $this->lastName,
             'email' => $this->email,
         ]);
 
-        Http::withToken('1|4hOjHuSZBHFlMtftXYeAJ3jTfQhdUdEiaF5GGmy6')->put('http://192.168.0.5:8080/api/patient/' . $this->patientID, [
+        Http::put('http://latoya.fimijm.com/eprescriptionapi/api/patient/' . $this->patientID, [
             'user_id' => $this->userID,
             'mx_no' => $this->mxNo,
             'dob' => $this->dob,
@@ -123,36 +123,28 @@ class Patients extends Component
         
         $this->history = true;
         // dd($id);
-        $this->historyData = Http::withToken('1|4hOjHuSZBHFlMtftXYeAJ3jTfQhdUdEiaF5GGmy6')->get('http://192.168.0.5:8080/api/history/' . $id)->json();
-        // dd($this->historyData['Patient']['allergies']);
-        $this->allergies = $this->historyData['Patient']['allergies'] ?? "";
-        $this->illness = $this->historyData['Patient']['health_conditions'] ?? "";
-        if ($this->historyData['Patient']['pregnant_nursing'] ?? "") {
-            $this->pregnant = "Yes";
-        }
-        $this->pregnant = "No";
-        $this->medicalHistoryId = $id;
-        $this->patientId = $this->historyData['Patient']['id']?? "";
+        $this->historyData = Http::get('http://latoya.fimijm.com/eprescriptionapi/api/patient/' . $id)->collect();
+        // dd($this->historyData['Patient']['0']['medical_history']);
+        $this->allergies = $this->historyData['Patient']['0']['medical_history']['allergies'] ?? "";
+        $this->illness = $this->historyData['Patient']['0']['medical_history']['health_conditions'] ?? "";
+        $this->pregnant = $this->historyData['Patient']['0']['medical_history']['pregnant_nursing'] ?? "";
+        $this->medicalHistoryId = $this->historyData['Patient']['0']['medical_history']['id'] ?? "";
+        $this->patientId = $this->historyData['Patient']['0']['id'] ?? "";
     }
 
     public function updateMedicalHistory()
     {
-    //    dd($this->patientId);
-        if (is_null($this->medicalHistoryId)) {
-            $newPatientId = Http::withToken('1|4hOjHuSZBHFlMtftXYeAJ3jTfQhdUdEiaF5GGmy6')->get('http://192.168.0.5:8080/api/patient/')->json();
-            $data = end($newPatientId);
-            $userId = reset($data);
-            Http::withToken('1|4hOjHuSZBHFlMtftXYeAJ3jTfQhdUdEiaF5GGmy6')->post('http://192.168.0.5:8080/api/history',[
-                'patient_id' => $userId,
+        // dd($this->medicalHistoryId);
+        if (empty($this->medicalHistoryId)) {
+            Http::post('http://latoya.fimijm.com/eprescriptionapi/api/history', [
+                'patient_id' => $this->patientId,
                 'allergies' => $this->allergies,
                 'health_conditions' => $this->illness,
                 'pregnant_nursing' => $this->pregnant
             ]);
         } else {
-            $history = Http::withToken('1|4hOjHuSZBHFlMtftXYeAJ3jTfQhdUdEiaF5GGmy6')->get('http://192.168.0.5:8080/api/history/' . $this->medicalHistoryId)->json();
-            // dd($history['Patient']['patient_id']);
-            Http::withToken('1|4hOjHuSZBHFlMtftXYeAJ3jTfQhdUdEiaF5GGmy6')->put('http://192.168.0.5:8080/api/history/' . $this->medicalHistoryId, [
-                'patient_id' => $history['Patient']['patient_id'],
+            Http::put('http://latoya.fimijm.com/eprescriptionapi/api/history/' . $this->medicalHistoryId, [
+                'patient_id' => $this->patientId,
                 'allergies' => $this->allergies,
                 'health_conditions' => $this->illness,
                 'pregnant_nursing' => $this->pregnant
@@ -163,7 +155,7 @@ class Patients extends Component
 
     public function render()
     {
-        $patient = Http::withToken('1|4hOjHuSZBHFlMtftXYeAJ3jTfQhdUdEiaF5GGmy6')->get('http://192.168.0.5:8080/api/patient')->collect();
+        $patient = Http::withToken('1|4hOjHuSZBHFlMtftXYeAJ3jTfQhdUdEiaF5GGmy6')->get('http://latoya.fimijm.com/eprescriptionapi/api/patient')->collect();
         //    dd($patient);
         return view('livewire.admin.patients', ['patient' => $patient,]);
     }
